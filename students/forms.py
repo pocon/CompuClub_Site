7c23dedeef5011fd2a235aa96965773c21411a8d
Patrick O'Connell <paddymelon@padmel.com>
2012-09-11 20:58:36 +1000

    Initial Commit

---
+++ students/forms.py
@@ -0,0 +1,1 @@
+class SettingsForm(forms.Form)
---
+++ students/views.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.decorators import login_required
+
+def UserCP(request):
+    if request.method == 'POST':
+        pass
+
+    else:
+        
