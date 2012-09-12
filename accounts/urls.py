6ae4010d1a7f6ae3493c1232ae918b001ed3ded3
Peter <peterarenot@gmail.com>
2012-09-12 18:41:24 +1000

    Added accounts and added urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,11 +6,12 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'CompuClub_Site.views.home', name='home'),
-    # url(r'^CompuClub_Site/', include('CompuClub_Site.foo.urls')),
-    url(r'^fixtures/', include('CompuClub_Site.fixtures.urls')),
-    url(r'^accounts/', include('CompuClub_Site.accounts.urls')),
-    url(r'^$', 'CompuClub_Site.views.home'),
+    # url(r'^$', 'fixtures.views.home', name='home'),
+    # url(r'^fixtures/', include('fixtures.foo.urls')),
+
+    url(r'^logout/^$', 'accounts.views.logout'),
+    url(r'^login/^$', 'accounts.views.login'),
+    url(r'^register/^$', 'accounts.views.register'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
---
+++ accounts/views.py
@@ -0,0 +1,6 @@
+from fixtures.models import *
+from django.shortcuts import get_object_or_404, render_to_response, redirect
+from django.template import Context, loader, RequestContext
+from django.contrib.auth.decorators import login_required
+
+# Write account views here
