625f80b98e7b69c14b5daf35bf2208686065d695
Peter <peterarenot@gmail.com>
2012-09-13 18:03:23 +1000

    Added login/username

--- CompuClub_Site/views.py
+++ CompuClub_Site/views.py
@@ -4,4 +4,4 @@ from django.template import Context, loader, RequestContext
 from django.contrib.auth.decorators import login_required
 
 def home(request):
-    return render_to_response('main/index.html')
+    return render_to_response('main/index.html', {'uname': request.user.username, 'isauth': request.user.is_authenticated(),})
