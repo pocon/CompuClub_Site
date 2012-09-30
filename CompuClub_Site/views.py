effef654e170e48d4d166d51f22919e8f477c3c2
Peter <peterarenot@gmail.com>
2012-10-01 00:34:09 +1000

    Added stuff

--- CompuClub_Site/views.py
+++ CompuClub_Site/views.py
@@ -1,7 +1,5 @@
-from fixtures.models import *
 from django.shortcuts import get_object_or_404, render_to_response, redirect
 from django.template import Context, loader, RequestContext
-from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render_to_response('main/index.html', {'uname': request.user.username, 'isauth': request.user.is_authenticated(),})
