216e517fe7a1fd645bf655ebf5e48cd3c442edd6
Patrick O'Connell <paddymelon@padmel.com>
2012-10-28 17:13:01 +1100

    Minor URLs change

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,9 +9,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'fixtures.views.home', name='home'),
     # url(r'^fixtures/', include('fixtures.foo.urls')),
 
-    url(r'^logout/^$', 'accounts.views.logout'),
-    url(r'^login/^$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}),
-    url(r'^register/^$', 'accounts.views.register'),
+    url(r'^logout/$', 'accounts.views.logout'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}),
+    url(r'^register/$', 'accounts.views.register'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
